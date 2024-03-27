4740e938a0ad4547e3236c6aee251cd5c065b5b6
Bikash Mishra <mishra.bikash002@gmail.com>
2024-03-27 13:03:00 +0530

    use cassandra

--- pkg/infra/cassandra.go
+++ pkg/infra/cassandra.go
@@ -12,6 +12,26 @@ var CassandraSession *gocql.Session
 
 func NewCassandraSession(config *config.Config) (*gocql.Session, error) {
 	cluster := gocql.NewCluster(common.GetServerAddrs(config.Cassandra.Hosts)...)
+	cluster.Port = config.Cassandra.Port
+	cluster.Keyspace = config.Cassandra.Keyspace
+	cluster.Consistency = gocql.Quorum
+	cluster.RetryPolicy = &gocql.SimpleRetryPolicy{
+		NumRetries: 3,
+	}
+	cluster.Authenticator = gocql.PasswordAuthenticator{
+		Username: config.Cassandra.User,
+		Password: config.Cassandra.Password,
+	}
+	cluster.DefaultIdempotence = false
+	// number of connections per host
+	cluster.NumConns = 3
+	CassandraSession, err := cluster.CreateSession()
+	return CassandraSession, err
+}
+
+// use this when using scylladb
+func NewCassandraSession1(config *config.Config) (*gocql.Session, error) {
+	cluster := gocql.NewCluster(common.GetServerAddrs(config.Cassandra.Hosts)...)
 	// cluster.Port = config.Cassandra.Port
 	cluster.Keyspace = config.Cassandra.Keyspace
 	cluster.Consistency = gocql.Quorum
